88d61ea0db4ce1573c8eddb717fa342ec32a9d1e
Pragnya_Swain <pragnyaps1109@gmail.com>
2024-12-12 14:44:40 +0530

    backend skeleton done

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.post("/todo", (req, res) => {});
+
+app.get("/todos", (req, res) => {});
+
+app.put("/completed", (req, res) => {});
+
+app.listen(PORT, () => {
+  [console.log(`Server is running on PORT ${PORT}`)];
+});
