45c08f286bb94029249347c203bc3d103692029c
Pragnya_Swain <pragnyaps1109@gmail.com>
2024-12-12 14:56:42 +0530

    added validation logic

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,36 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.post("/todo", (req, res) => {});
+app.post("/todo", (req, res) => {
+    const creatPayload = req.body,
+    const parsedPayload = createTodo.safeParse(creatPayload)
+    if(!parsedPayload.success){
+        res,status(411).json({
+            msg:'You sent the wrong inputs',
+        })
+        return;
+    }
+});
 
-app.get("/todos", (req, res) => {});
+app.get("/todos", (req, res) => {
 
-app.put("/completed", (req, res) => {});
+});
+
+app.put("/completed", (req, res) => {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload)
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"You sent wrong inputs",
+        })
+        return;
+    }
+});
 
 app.listen(PORT, () => {
   [console.log(`Server is running on PORT ${PORT}`)];
